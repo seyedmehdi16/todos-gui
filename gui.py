12c774778457e27e3ae96e7851d984d8ac2f7651
seyedmehdi16 <seyed.mehdi.3303@gmail.com>
2026-09-14 11:32:30 +0330

    Change theme, and clock, handle edit and coplete error

--- gui.py
+++ gui.py
@@ -1,11 +1,13 @@
-from unittest import case
 
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme('LightBrown10')
+clock = sg.Text(time.strftime("%b %d, %Y %H:%M:%S"), key='clock'),
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10, button_color=("blue", "white"), key="add_button")
 exit_button = sg.Button("Exit")
 
 list_box = sg.Listbox(values=functions.get_todos("todos.txt"), key="todos",
@@ -15,14 +17,15 @@ edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 
 window = sg.Window('To-Do List', layout=[
+    [clock],
     [label],
-   [ input_box,
-    add_button],
+    [input_box,add_button],
     [list_box, edit_button, complete_button],
     [exit_button]
 ], font=('8514oem', 23))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=1000)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(event)
     print(values)
     print(values['todos'])
@@ -34,26 +37,33 @@ while True:
             functions.write_todos("todos.txt", todos)
             window['todos'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos("todos.txt")
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos("todos.txt", todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos("todos.txt")
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos("todos.txt", todos)
+                window['todos'].update(values=todos)
+
+            except IndexError:
+                sg.popup("Please select an item first", font=('8514oem', 23))
         case "todos":
             window['todo'].update(value=values['todos'][0])
         case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos("todos.txt")
-            todos.remove(todo_to_complete)
-            functions.write_todos("todos.txt", todos)
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos("todos.txt")
+                todos.remove(todo_to_complete)
+                functions.write_todos("todos.txt", todos)
 
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first", font=('8514oem', 23))
         case "Exit":
-            break
+                break
         case sg.WINDOW_CLOSED:
             break
 
